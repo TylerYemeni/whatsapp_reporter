c8ba1be66f3fe67f4aa6c45d1c309f1af933064b
TylerYemeni <leomessi111909@gmail.com>
2025-04-17 02:49:53 +0300

    Create report_engine.py

---
+++ report_engine.py
@@ -0,0 +1,5 @@
+def send_report(target_number, from_number, proxy=None):
+    # تمثيل بلاغ على رقم واتساب
+    print(f"[+] Sending report from {from_number} to {target_number}")
+    # هنا تضع أكواد API أو واجهة ويب البلاغ
+    # يمكن لاحقًا ربط Google Form أو صفحة دعم واتساب
